aacd508be8af9ad2cb01b0e707cf7274d4218582
Clark Stephenson <clarkstephenson90@gmail.com>
2023-06-01 13:24:49 -0600

    added more lines

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ app.get('/cameron', (req,res) => {res.send("Hello Cameron")});
 
 app.get('/garret', (req,res) => {res.send("Hello Garret")});
 
+app.get('/clark', (req,res) => {res.send("Hello Clark")});
+
+app.get('/harrison', (req,res) => {res.send("Hello Harrison")});
+
+app.get('/americo', (req,res) => {res.send("Hello Americo")});
+
 app.listen(port, ()=>console.log("Listening"));
