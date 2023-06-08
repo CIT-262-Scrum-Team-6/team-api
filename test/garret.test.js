19641ad692685435f68d21db71cb30ffec53e04b
AmericoKutomi <americo@embra.com.br>
2023-06-08 17:38:58 -0300

    my local changes

--- test/garret.test.js
+++ test/garret.test.js
@@ -1,5 +1,7 @@
+
+
 const response = await fetch('http://localhost:3000/garret');
 
 const first_name = await response.text();
 
-expect.(first_name).toBe("garret");
+expect(first_name).toBe("garret");
